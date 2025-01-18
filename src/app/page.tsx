2c942419d5fe3722d562dec2552f8250ace3bc2a
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-18 20:41:56 +0600

    Navbar completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 const HomePage = () => {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant={"secondary"}>Click me</Button>
-      <ModeToggle />
+      <h1>home</h1>
     </div>
   );
 };
