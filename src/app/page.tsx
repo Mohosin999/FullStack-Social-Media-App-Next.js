6565ca814c45b93944546ae3fbd98b0bc773a54d
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-19 18:26:15 +0600

    whotofollow section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 const HomePage = async () => {
@@ -6,12 +7,17 @@ const HomePage = async () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      {/* Center content area */}
-      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      {/* Middle content area */}
+      <div className="lg:col-span-6">
+        {/* Create post area */}
+        {user ? <CreatePost /> : null}
+
+        {/* Show all posts */}
+      </div>
 
       {/* Who to follow you */}
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        WhoToFollow
+        <WhoToFollow />
       </div>
     </div>
   );
