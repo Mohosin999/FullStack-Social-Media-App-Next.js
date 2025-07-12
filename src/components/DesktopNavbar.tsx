a94314f0129bbcc3e32b065b328d1b23d71e790a
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-07-12 22:29:42 +0600

    refactor: withing DesktopNavbar.tsx file

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -1,3 +1,4 @@
+"use server";
 import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -11,14 +12,13 @@ const DesktopNavbar = async () => {
   return (
     <div className="hidden md:flex items-center space-x-4">
       <ModeToggle />
-
       <Button variant="ghost" className="flex items-center gap-2" asChild>
         <Link href="/">
           <HomeIcon className="w-4 h-4" />
           <span className="hidden lg:inline">Home</span>
         </Link>
       </Button>
-
+      9.lo
       {user ? (
         <>
           <Button variant="ghost" className="flex items-center gap-2" asChild>
